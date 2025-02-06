97589d68f214766f41338f0d3377586487b9b1cb
PehansaDharmapala <pehansaranumi86@gmail.com>
2025-02-06 16:02:17 +0530

    Database created Repositories fixed errors

--- backend/src/main/java/com/sched_ease/backend/database/repositories/LecturerAvailabilityRepository.java
+++ backend/src/main/java/com/sched_ease/backend/database/repositories/LecturerAvailabilityRepository.java
@@ -3,5 +3,5 @@ package com.sched_ease.backend.database.repositories;
 import com.sched_ease.backend.database.entities.LecturerAvailability;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LectuerAvailabilityRepository extends JpaRepository<LecturerAvailability, Long> {
+public interface LecturerAvailabilityRepository extends JpaRepository<LecturerAvailability, Long> {
 }
--- backend/src/main/java/com/sched_ease/backend/database/repositories/SDGPLecturerRepository.java
+++ backend/src/main/java/com/sched_ease/backend/database/repositories/SDGPLecturerRepository.java
@@ -2,5 +2,5 @@ package com.sched_ease.backend.database.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SDGPLectutrerRepository extends JpaRepository<SDGPLectutrerRepository, Long> {
+public interface SDGPLecturerRepository extends JpaRepository<SDGPLecturerRepository, Long> {
 }
