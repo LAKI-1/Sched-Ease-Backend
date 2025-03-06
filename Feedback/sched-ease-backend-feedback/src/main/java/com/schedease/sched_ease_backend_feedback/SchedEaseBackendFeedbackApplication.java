1fc4456dc6251544423fb4d8b8b9b4d6f78b49ea
Lakindu Induwara Samarasinghe <lakinduinduwara0916@gmail.com>
2025-03-06 14:34:28 +0530

    Initial Commits

---
+++ Feedback/sched-ease-backend-feedback/src/main/java/com/schedease/sched_ease_backend_feedback/SchedEaseBackendFeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.schedease.sched_ease_backend_feedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedEaseBackendFeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedEaseBackendFeedbackApplication.class, args);
+	}
+
+}
---
+++ Feedback/sched-ease-backend-feedback/src/test/java/com/schedease/sched_ease_backend_feedback/SchedEaseBackendFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schedease.sched_ease_backend_feedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedEaseBackendFeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
