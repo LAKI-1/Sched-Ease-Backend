da69dc8c184fd33f83ec63da648e518c8b0dff65
Faaeq Fazal <faaeqavl@gmail.com>
2025-03-23 16:54:52 +0530

    Made a few changes in the controller classes

--- backend/src/main/java/com/sched_ease/backend/controllers/HallController.java
+++ backend/src/main/java/com/sched_ease/backend/controllers/HallController.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/halls")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000")
 public class HallController {
 
     @Autowired
--- backend/src/main/java/com/sched_ease/backend/controllers/StudentController.java
+++ backend/src/main/java/com/sched_ease/backend/controllers/StudentController.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/students")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000")
 public class StudentController {
 
     @Autowired
